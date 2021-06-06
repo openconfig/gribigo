ed12d4b9d95f566374552e84122b5ac0a9b4072e
Rob Shakir <robjs@google.com>
2021-06-06 15:35:00 -0700

    Create compliance test library, add error check support
      * (M) chk/chk.go
        - Add support for checking whether the received errors from the
          server contain a particular error.
      * (A) compliance/compliance.go
      * (A) compliance/compliance_test.go
        - Create a compliance testing library for tests that are relevant
          to other implementations.
      * (M) fluent/fluent.go
      * (M) fluent/fluent_test.go
        - Remove compliance tests from fluent_test.go
        - Implement builder for Modify errors.
      * (A) testcommon/testcommon.go
        - Move testServer into a common package.

--- chk/chk.go
+++ chk/chk.go
@@ -3,7 +3,11 @@
 // values.
 package chk
 
-import "github.com/openconfig/gribigo/client"
+import (
+	"github.com/openconfig/gribigo/client"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+)
 
 // HasElectionID returns true if the result queue supplied contains an update with the election ID
 // with the value of the uint128{low,high} value specified.
@@ -18,9 +22,28 @@ func HasElectionID(res []*client.OpResult, low, high uint64) bool {
 	return false
 }
 
+// HasSuccessfulSessionParams checks whether the res results array contains a session parameters
+// result that is successful.
 func HasSuccessfulSessionParams(res []*client.OpResult) bool {
 	for _, r := range res {
 		if v := r.SessionParameters; v != nil {
+			return true
+		}
+	}
+	return false
+}
+
+// HasRecvClientErrorWithStatus checks whether the supplied ClientErr ce containa  status with
+// the code and details set to the values supplied in want.
+func HasRecvClientErrorWithStatus(ce *client.ClientErr, want *status.Status) bool {
+	for _, e := range ce.Recv {
+		s, ok := status.FromError(e)
+		if !ok {
+			continue
+		}
+		ns := s.Proto()
+		ns.Message = "" // blank out message so that we don't compare it.
+		if proto.Equal(ns, want.Proto()) {
 			return true
 		}
 	}

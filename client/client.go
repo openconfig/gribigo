086e709c449f40ddd398ee9c696a7ee0e3da9544
Rob Shakir <robjs@google.com>
2021-06-07 20:26:34 -0700

    remove dbg print.

--- client/client.go
+++ client/client.go
@@ -423,7 +423,7 @@ func (p *pendingQueue) Len() int {
 	if p.Election != nil {
 		i++
 	}
-	fmt.Printf("--> %d, %+v\n", i, p)
+
 	return i + len(p.Ops)
 }
 
@@ -607,7 +607,6 @@ func (c *Client) isConverged() bool {
 	defer c.qs.sendMu.RUnlock()
 	c.qs.pendMu.RLock()
 	defer c.qs.pendMu.RUnlock()
-	fmt.Printf("s: %d, r: %d, send: %v, pend: %v\n", c.sendInProgress.Load(), c.recvInProgress.Load(), c.qs.sendq, c.qs.pendq)
 	return len(c.qs.sendq) == 0 && c.qs.pendq.Len() == 0
 }
 

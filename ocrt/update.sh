0513403feed6963e51bb9c705d8c823bf3ebfa2b
Rob Shakir <robjs@google.com>
2021-06-11 02:04:19 -0700

    Add an initial implementation of a resolved-RIB.
    Background -- we want the gRIBI server to be able build both a RIB that
    is efficient for it to do gRIBI operations with, as well as a software
    emulated 'resolved RIB' / 'FIB' whereby we can say something like "where
    will this packet go based on what gRIBI is programmed?". This package
    starts this implementation.
    
      * (M) aft/oc.go
        - Update generated code.
      * (M) go.mod
      * (M) go.sum
        - New dependencies on kentik/patricia
      * (A) ocrt/oc.go
      * (A) ocrt/update.sh
        - Generated routing specific OpenConfig Go structs.
      * (A) ocrt/yang/deviations.yang
        - Deviations to reduce the size of the generated oc.go.
      * (M) rib/rib.go
        - Fix handling of implicit replace.
      * (A) sysrib/sysrib.go
      * (A) sysrib/sysrib_test.go
        -  Initial system RIB implementation. This package currently parses
            OpenConfig to build a list of all of the connected routes that
            are on the system.

---
+++ ocrt/update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Hack to ensure that if we are running on OS X with a homebrew installed
+# GNU sed then we can still run sed.
+runsed() {
+  if hash gsed 2>/dev/null; then
+    gsed "$@"
+  else
+    sed "$@"
+  fi
+}
+
+git clone https://github.com/openconfig/public.git
+go get github.com/openconfig/ygot@latest
+go install github.com/openconfig/ygot/generator
+
+generator -path=public -output_file=oc.go \
+    -package_name=oc -generate_fakeroot -fakeroot_name=device -compress_paths=true \
+    -shorten_enum_leaf_names \
+    -prefer_operational_state \
+    -trim_enum_openconfig_prefix \
+    -typedef_enum_with_defmod \
+    -enum_suffix_for_simple_union_enums \
+    -exclude_modules=ietf-interfaces,openconfig-acl,openconfig-routing-policy \
+    -generate_getters \
+    -generate_leaf_getters \
+    -generate_delete \
+    -generate_append \
+    public/release/models/interfaces/openconfig-interfaces.yang \
+    public/release/models/interfaces/openconfig-if-ip.yang \
+    public/release/models/network-instance/openconfig-network-instance.yang \
+    yang/deviations.yang
+
+git clone -b v0.4 https://github.com/mbrukman/autogen.git
+autogen/autogen --no-code --no-tlc -c "The OpenConfig Contributors" -l apache -i oc.go
+gofmt -w -s oc.go
+rm -rf autogen public

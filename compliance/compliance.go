b56e19939287292435c3c81e875a26f564035c8a
Rob Shakir <robjs@google.com>
2021-06-16 15:34:41 -0700

    Fix compliance tests by merging NHG/NH fluent code.
      * (M) compliance/compliance.go
        - Ensure that the compliance test specifies NHG and NHs.

--- compliance/compliance.go
+++ compliance/compliance.go
@@ -95,6 +95,8 @@ func AddIPv4EntrySuccess(addr string, t testing.TB) {
 	c := fluent.NewClient()
 	c.Connection().WithTarget(addr).WithRedundancyMode(fluent.ElectedPrimaryClient).WithInitialElectionID(0, 1).WithPersistence()
 	c.Start(context.Background(), t)
+	c.Modify().AddEntry(t, fluent.NextHopEntry().WithNetworkInstance(server.DefaultNIName).WithIndex(1))
+	c.Modify().AddEntry(t, fluent.NextHopGroupEntry().WithNetworkInstance(server.DefaultNIName).WithID(42).AddNextHop(1, 1))
 	c.Modify().AddEntry(t, fluent.IPv4Entry().WithPrefix("1.1.1.1/32").WithNetworkInstance(server.DefaultNIName).WithNextHopGroup(42))
 	c.StartSending(context.Background(), t)
 	err := c.Await(context.Background(), t)

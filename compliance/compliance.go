392f6f0317b795f1219d6110eb92bf83eb5776c6
Rob Shakir <robjs@google.com>
2021-06-19 07:34:22 -0700

    Add licensing to all files. (#32)
     * (M) ...
       - Add Apache 2.0 license to files.
     * (A) CONTRIBUTORS
        - ACK folks that wrote and reviewed gribigo code thus far.

--- compliance/compliance.go
+++ compliance/compliance.go
@@ -1,3 +1,17 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package compliance encapsulates a set of compliance tests for gRIBI. It is a test only
 // library. All tests are of the form func(address string, t testing.TB) where the address
 // is the address that should be dialed for the test. The testing.TB object is used to report

13aac3cf30e07f837f6145d06173647bbeb6e9ee
Rob Shakir <robjs@google.com>
2021-06-23 13:02:32 -0700

    Plumb the gRIBI server -> sysRIB.
      * (M) compliance/compliance.go
        - Make the compliance test case actually resolve an IPv4
          prefix to an egress interface
      * (M) device/device.go
      * (M) device/device_test.go
        - Connect the gRIBI server to sysRIB so that entries that are
          installed are then installed into the sysRIB when they are
          resolved.
      * (M) fluent/fluent.go
        - Add support
      * (M) server/server.go
        - Rename hooks to be more intuitive.

--- device/device_test.go
+++ device/device_test.go
@@ -16,15 +16,43 @@ package device
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net"
 	"testing"
 
+	log "github.com/golang/glog"
+	"github.com/google/go-cmp/cmp"
 	"github.com/openconfig/gribigo/compliance"
+	"github.com/openconfig/gribigo/ocrt"
+	"github.com/openconfig/gribigo/sysrib"
 	"github.com/openconfig/gribigo/testcommon"
+	"github.com/openconfig/ygot/ygot"
 )
+
+type ribQuery struct {
+	NetworkInstance string
+	Prefix          *net.IPNet
+}
+
+func jsonDevice() []byte {
+	d := &ocrt.Device{}
+	d.GetOrCreateNetworkInstance("DEFAULT").Type = ocrt.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_DEFAULT_INSTANCE
+	d.GetOrCreateInterface("eth0").GetOrCreateSubinterface(1).GetOrCreateIpv4().GetOrCreateAddress("192.0.2.1").PrefixLength = ygot.Uint8(31)
+
+	j, err := ygot.Marshal7951(d, nil)
+	if err != nil {
+		panic(fmt.Sprintf("cannot create JSON, %v", err))
+	}
+	return j
+}
 
 func TestDevice(t *testing.T) {
 	devCh := make(chan string, 1)
 	errCh := make(chan error, 1)
+	ribCh := make(chan *ribQuery, 1)
+	ribErrCh := make(chan error)
+	ribResultCh := make(chan []*sysrib.Interface, 1)
 
 	creds, err := TLSCredsFromFile(testcommon.TLSCreds())
 	if err != nil {
@@ -34,17 +62,55 @@ func TestDevice(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	go func() {
-		d, err := New(ctx, creds)
+		d, err := New(ctx, creds, DeviceConfig(jsonDevice()))
 		if err != nil {
 			errCh <- err
 		}
 		devCh <- d.GRIBIAddr()
-		<-ctx.Done()
+
+		for {
+			select {
+			case qryPfx := <-ribCh:
+				ints, err := d.sysRIB.EgressInterface(qryPfx.NetworkInstance, qryPfx.Prefix)
+				if err != nil {
+					ribErrCh <- err
+				}
+				ribResultCh <- ints
+			case <-ctx.Done():
+				return
+			}
+		}
 	}()
 	select {
 	case err := <-errCh:
 		t.Fatalf("got unexpected error from device, got: %v", err)
 	case addr := <-devCh:
 		compliance.AddIPv4EntryRIBACK(addr, t)
+
+		_, cidr, err := net.ParseCIDR("1.1.1.1/32")
+		if err != nil {
+			t.Fatalf("cannot parse CIDR for destination, err: %v", err)
+		}
+
+		ribCh <- &ribQuery{NetworkInstance: "DEFAULT", Prefix: cidr}
+		select {
+		case err := <-ribErrCh:
+			t.Fatalf("cannot run RIB query, gotErr: %v", err)
+		case got := <-ribResultCh:
+			js, err := json.MarshalIndent(got, "", "  ")
+			if err != nil {
+				t.Fatalf("cannot marshal JSON response, %v", err)
+			}
+			log.Infof("got egress interface, %s", js)
+
+			want := []*sysrib.Interface{{
+				Name:         "eth0",
+				Subinterface: 1,
+			}}
+
+			if diff := cmp.Diff(got, want); diff != "" {
+				t.Fatalf("did not get expected egress interface, diff(-got,+want):\n%s", diff)
+			}
+		}
 	}
 }

961c0eb8779f0c2add83fcc06b1ffbb6f5ea83fb
Rob Shakir <robjs@google.com>
2021-06-29 17:04:57 -0700

    Add helper function to find the next-hops for a particular prefix. (#61)
      * (A) afthelper/afthelper.go
      * (A) afthelper/afthelper_test.go
       -  Add a helper function to take a RIB and determine the set of
          IP address next-hops from the AFT model. This allows us to
          take an input gRIBI entry and determine what next-hop IPs
          it uses, which then can be resolved to connected interfaces.

---
+++ afthelper/afthelper_test.go
@@ -0,0 +1,123 @@
+package afthelper
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/openconfig/gribigo/aft"
+	"github.com/openconfig/ygot/ygot"
+)
+
+const (
+	defName = "DEFAULT"
+)
+
+func TestNextHopAddrsForPrefix(t *testing.T) {
+
+	tests := []struct {
+		desc      string
+		inRIB     map[string]*aft.RIB
+		inNetInst string
+		inPrefix  string
+		want      map[string]*NextHopSummary
+		wantErr   bool
+	}{{
+		desc: "simple ipv4 to one NH",
+		inRIB: map[string]*aft.RIB{
+			defName: func() *aft.RIB {
+				r := &aft.RIB{}
+				r.GetOrCreateAfts().GetOrCreateIpv4Entry("8.8.8.8/32").NextHopGroup = ygot.Uint64(1)
+				r.GetOrCreateAfts().GetOrCreateNextHopGroup(1).GetOrCreateNextHop(1).Weight = ygot.Uint64(1)
+				r.GetOrCreateAfts().GetOrCreateNextHop(1).IpAddress = ygot.String("1.1.1.1")
+				return r
+			}(),
+		},
+		inNetInst: defName,
+		inPrefix:  "8.8.8.8/32",
+		want: map[string]*NextHopSummary{
+			"1.1.1.1": {
+				Weight:          1,
+				Address:         "1.1.1.1",
+				NetworkInstance: defName,
+			},
+		},
+	}, {
+		desc: "ipv4 with two NH",
+		inRIB: map[string]*aft.RIB{
+			defName: func() *aft.RIB {
+				r := &aft.RIB{}
+				r.GetOrCreateAfts().GetOrCreateIpv4Entry("8.8.8.8/32").NextHopGroup = ygot.Uint64(1)
+				r.GetOrCreateAfts().GetOrCreateNextHopGroup(1).GetOrCreateNextHop(1).Weight = ygot.Uint64(1)
+				r.GetOrCreateAfts().GetOrCreateNextHopGroup(1).GetOrCreateNextHop(2).Weight = ygot.Uint64(1)
+				r.GetOrCreateAfts().GetOrCreateNextHop(1).IpAddress = ygot.String("1.1.1.1")
+				r.GetOrCreateAfts().GetOrCreateNextHop(2).IpAddress = ygot.String("2.2.2.2")
+				return r
+			}(),
+		},
+		inNetInst: defName,
+		inPrefix:  "8.8.8.8/32",
+		want: map[string]*NextHopSummary{
+			"1.1.1.1": {
+				Address:         "1.1.1.1",
+				Weight:          1,
+				NetworkInstance: defName,
+			},
+			"2.2.2.2": {
+				Address:         "2.2.2.2",
+				Weight:          1,
+				NetworkInstance: defName,
+			},
+		},
+	}, {
+		desc:      "can't find network instance",
+		inRIB:     map[string]*aft.RIB{},
+		inNetInst: "fish",
+		wantErr:   true,
+	}, {
+		desc: "can't find ipv4 prefix",
+		inRIB: map[string]*aft.RIB{
+			defName: {},
+		},
+		inPrefix: "42.42.42.42/32",
+		wantErr:  true,
+	}, {
+		desc: "ipv4 with next-hop-group in another network instance",
+		inRIB: map[string]*aft.RIB{
+			defName: func() *aft.RIB {
+				r := &aft.RIB{}
+				v4 := r.GetOrCreateAfts().GetOrCreateIpv4Entry("1.2.3.4/32")
+				v4.NextHopGroup = ygot.Uint64(1)
+				v4.NextHopGroupNetworkInstance = ygot.String("VRF-1")
+				return r
+			}(),
+			"VRF-1": func() *aft.RIB {
+				r := &aft.RIB{}
+				r.GetOrCreateAfts().GetOrCreateNextHopGroup(1).GetOrCreateNextHop(2).Weight = ygot.Uint64(2)
+				r.GetOrCreateAfts().GetOrCreateNextHop(2).IpAddress = ygot.String("2.2.2.2")
+				return r
+			}(),
+		},
+		inPrefix:  "1.2.3.4/32",
+		inNetInst: defName,
+		want: map[string]*NextHopSummary{
+			"2.2.2.2": {
+				Weight:          2,
+				Address:         "2.2.2.2",
+				NetworkInstance: "VRF-1",
+			},
+		},
+	}}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			got, err := NextHopAddrsForPrefix(tt.inRIB, tt.inNetInst, tt.inPrefix)
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("did not get expected error, got: %v, wantErr? %v", err, tt.wantErr)
+			}
+
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Fatalf("did not get expected output, diff(-got,+want):\n%s", diff)
+			}
+		})
+	}
+}
